0a22f03dccf2232ced05ea803645871ef21930de
Matthew Peterson <matt2694@edu.eal.dk>
2017-11-30 10:48:38 +0100

    ????

--- Week48Exercise/WCFServiceLibrary/IService.cs
+++ Week48Exercise/WCFServiceLibrary/IService.cs
@@ -9,7 +9,7 @@ namespace WCFServiceLibrary
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IService
     {
         [OperationContract]
         string GetData(int value);
@@ -20,7 +20,7 @@ namespace WCFServiceLibrary
         // TODO: Add your service operations here
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // Use a data contract as illustrated in the sample below to add composite types to service operations. test
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WCFServiceLibrary.ContractType".
     [DataContract]
     public class CompositeType
